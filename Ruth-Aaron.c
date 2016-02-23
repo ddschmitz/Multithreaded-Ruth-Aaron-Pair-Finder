2a6815b1bf9fae56b0ebbdf75b8fb35bf6912406
ddschmitz <ddschmitz@pluto.dsu.edu>
2016-02-22 23:02:18 -0600

    Changed defined functions.
    There were two #defined functions to actual functions with better variable names so it made more sense.  Also commented and prettified the code.

--- Ruth-Aaron.c
+++ Ruth-Aaron.c
@@ -2,7 +2,7 @@
 	Author:  Darrin Schmitz
 	Date:    11/28/2015
 	
-	This is a multithreaded program that finds Ruth-Aaron Pairs.
+	A multithreaded Ruth-Aaron pair finder written in C.
 */
 
 #include <stdio.h>
@@ -12,26 +12,25 @@
 #include <string.h>
 #include <pthread.h>
 
-#define PRIMES 10000000    // Size of seive 
-#define THNUMS 15          // Number of threads
+#define PRIMES 10000000    // Size of sieve.
+#define THNUMS 15          // Number of threads.
 #define PAIRS 1000000      // How many pairs to check. 
-
-#define BLOCK_LOW(id, p, n)	((id)*(n)/(p))
-#define BLOCK_HIGH(id, p, n)	(BLOCK_LOW((id)+1,p,n)-1)
 
 // Thread parametes
 struct ThreadParams
 { 
-	int id;				   // id
-	int low;			   // start
-	int high;		   	   // end
-	int *structprimes;     // each thread's seive
+	int id;				   // Each thread's id.
+	int low;			   // Where each thread begins checking for pairs.
+	int high;		   	   // Where each thread stops checking for pairs.
+	int *structprimes;     // Each thread's sieve.
 };
 
 // Function prototypes
 void *Work(void *arg);
+int lowerBound(int threadId, int thnums, int pairs);
+int higherBound(int threadId, int thnums, int pairs);
 
-int main()
+int main(void)
 {
 //-----Timing the Program-----
 	clock_t begin;
@@ -44,20 +43,17 @@ int main()
 	clock_gettime(CLOCK_MONOTONIC, &start);
 //-----------------------------
 
-
 	struct ThreadParams ptharg[THNUMS];
-	int i = 0; //First number in pair
-	int i2 = 1; //Second number in pair
 	pthread_t tid;
-	void *status;
+    void *status;
 
-	unsigned long long int a,b;
+    // Create a Sieve of Eratosthenese which we will use to check if a number is prime.
+    unsigned long long int a,b;
     int *primes;
-	primes = malloc(sizeof(int)*PRIMES);
-	for(a = 2; a <PRIMES; a++)//Set every element in the array to 1
+    primes = malloc(sizeof(int)*PRIMES);
+    for(a = 2; a <PRIMES; a++)
         primes[a] = 1;
-	
-	for(a = 2; a < PRIMES; a++)//Place a 0 in the elements that are not primes
+    for(a = 2; a < PRIMES; a++)
         if(primes[a])
             for(b = a; a * b < PRIMES; b++)
                 primes[a * b] = 0;
@@ -65,16 +61,15 @@ int main()
 	for (a = 0; a < THNUMS; a++)// Copy the sieve to each threads array.
 		ptharg[a].structprimes = malloc(sizeof(int)*PRIMES);
 	
-			
+	int i;
 	for (i = 0; i < THNUMS; i++)
 	{
 		ptharg[i].id = i;
-		ptharg[i].low = BLOCK_LOW(i, THNUMS, PAIRS);
-		ptharg[i].high = BLOCK_HIGH(i, THNUMS, PAIRS);
+		ptharg[i].low = lowerBound(i, THNUMS, PAIRS);// Where the specified thread (with id i) will start checking.
+		ptharg[i].high = higherBound(i, THNUMS, PAIRS);// Where the specified thread (with id i) will stop checking.
 		memcpy(ptharg[i].structprimes, primes, sizeof(int)*PRIMES);
 
 		printf("ID=%d   LOW=%d   HIGH=%d\n", ptharg[i].id, ptharg[i].low, ptharg[i].high);
-		
 		
 		if (pthread_create(&tid, NULL, Work, (void*)&ptharg[i]) != 0)
 		{
@@ -82,10 +77,9 @@ int main()
 			return -1;
 		}
 
-		//pthread_join(tid, &status);
-		if (i == THNUMS-1) pthread_join(tid, &status);
+		if (i == THNUMS-1)
+            pthread_join(tid, &status);
 	}
-
 	
 //-----Timing the Program-----
 	end = clock();
@@ -101,51 +95,60 @@ int main()
 	return 0;
 }
 
+int lowerBound(int threadId, int thnums, int pairs)
+{
+	return threadId * pairs / thnums;
+}
+
+int higherBound(int threadId, int thnums, int pairs)
+{
+	return lowerBound(threadId + 1, thnums, pairs) - 1;
+}
+
 void *Work(void *arg)
 {
-	int x;
-	int y;
+	int pair1;// First number in pair.
+	int pair2;// Second number in pair.
+	int i;
 	int total1;
 	int total2;
 	int other_factor;
 	
-	struct ThreadParams *pm = (struct ThreadParams*)arg; //Making local Struct and assigning it to passed in struct. 
+	struct ThreadParams *pm = (struct ThreadParams*)arg; // Making local Struct and assigning it to passed in struct. 
 	
-	int i;
-	int i2;
-	for(i = pm->low, i2 = i++; i < pm->high; i++, i2++)
+	for(pair1 = pm->low, pair2 = pair1++; pair1 < pm->high; pair1++, pair2++)
 	{	
 		total1 = 0;
 		total2 = 0;
-		for(x = 2; x <= sqrt(i) + 1; x++)
+		for(i = 2; i <= sqrt(pair1) + 1; i++)
 		{
-			if(i % x == 0) //if it is a factor
+			if(pair1 % i == 0)// If it is a factor.
 			{
-				other_factor = i / x; 
+				other_factor = pair1 / i;// Find the other factor using the first one.
 				
-				if(pm->structprimes[x] == 1)//if its in the seive its a prime
-					total1 = x + total1;
+				if(pm->structprimes[i] == 1)// If its in the sieve its a prime.  Also add it to the total.  Do the same with the other factor.
+					total1 = i + total1;
 					
-				if(pm->structprimes[other_factor] == 1)
+				if(pm->structprimes[other_factor] == 1)// Do the same with the other factor.
 					total1 = other_factor + total1;
 			}
 		}
 
-		for(x = 2; x <= sqrt(i2) + 1; x++)
+		for(i = 2; i <= sqrt(pair2) + 1; i++)
 		{
-			if(i2 % x == 0) //if it is a factor
+			if(pair2 % i == 0)// If it is a factor.
 			{
-				other_factor = i2 / x;
+				other_factor = pair2 / i;// Find the other factor using the first one.
 				
-				if(pm->structprimes[x] == 1)//if its in the seive its a prime
-					total2 = x + total2;
+				if(pm->structprimes[i] == 1)// If its in the sieve its a prime.  Also add it to the total.
+					total2 = i + total2;
 				
-				if(pm->structprimes[other_factor] == 1)
+				if(pm->structprimes[other_factor] == 1)// Do the same with the other factor.
 					total2 = other_factor + total2;
 			}
 		}
 		
 		if(total1 == total2)
-			printf("From thread ID: %d  | These two are a Ruth-Aaron pair: %d, %d\n", pm->id, i2, i); // Gets switched some how?
+			printf("From thread ID: %3d | These two are a Ruth-Aaron pair: %d, %d\n", pm->id, pair2, pair1); // Gets switched some how?
 	}
 }
